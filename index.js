e3a9353d8a5a67b0322761cf2534714d733ac3de
Lahrasab Razman <s3neo36@gmail.com>
2023-02-15 19:05:28 +0500

    final code

--- index.js
+++ index.js
@@ -6,40 +6,17 @@ const { response } = require('express');
 const PORT = 8000;
 
 const newsPapers = [
-    // {
-    //     paper: 'AP News',
-    //     address: 'https://apnews.com/hub/pakistan'
-    // },
-
     {
         paper: 'ARY News',
         address: 'https://arynews.tv/category/pakistan/'
-    },
-
-    // {
-    //     paper: 'NPR',
-    //     address: 'https://www.npr.org/search/?query=pakistan&page=1&range%5BlastModifiedDate%5D%5Bmin%5D=1675814400'
-    // },
-
-    // {
-    //     paper: 'Reuters',
-    //     address: 'https://www.reuters.com/news/archive/pakistan'
-    // }
+    }
 ]
-
-
-
-
-
 
 const exp = express();
 
 exp.get('/', (req, res) => {
     res.send('welcome to the pakistan news api')
 })
-
-
-
 
 
 exp.get('/pakistan', (req, res) => {
@@ -53,7 +30,7 @@ exp.get('/pakistan', (req, res) => {
 
 
                 $('div[class="td-module-meta-info"]', rawData).each(function () {
-                    console.log("ok");
+                    // console.log("ok");
                     
                     const title = $(this).find('h3').text();
                     const url = $(this).find('a').attr('href');
@@ -68,25 +45,6 @@ exp.get('/pakistan', (req, res) => {
             })
     })
 
-
-    // axios.get('https://arynews.tv/?s=climate')
-    //     .then((response) => {
-    //         const rawData = response.data;
-    //         const $ = cheerio.load(rawData);
-
-    //         $('a:contains("climate")', rawData).each(function () {
-    //             const title = $(this).text()
-    //             const url = $(this).attr('href')
-    //             articles.push({
-    //                 title,
-    //                 url,
-    //             })
-    //         })
-    //         return res.json(articles)
-    //     })
 })
 
 exp.listen(PORT, () => console.log(`Server is listening on port ${PORT}`))
-
-
-
